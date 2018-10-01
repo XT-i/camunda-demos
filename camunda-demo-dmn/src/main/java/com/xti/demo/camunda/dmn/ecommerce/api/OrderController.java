b4c1c1bc5c0b37df8f2160e0645a64cfbc0098c0
Günther Van Roey <gunther.vanroey@xt-i.com>
2018-10-01 14:13:09 +0200

    minor code cleanup

--- camunda-demo-dmn/src/main/java/com/xti/demo/camunda/dmn/ecommerce/api/OrderController.java
+++ camunda-demo-dmn/src/main/java/com/xti/demo/camunda/dmn/ecommerce/api/OrderController.java
@@ -3,20 +3,16 @@ package com.xti.demo.camunda.dmn.ecommerce.api;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.ws.rs.Produces;
-
 import org.camunda.bpm.dmn.engine.DmnDecisionResult;
 import org.camunda.bpm.engine.DecisionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.xti.demo.camunda.dmn.util.GenericResult;
 import com.xti.demo.camunda.dmn.util.OrderHandlingResult;
@@ -26,9 +22,8 @@ import com.xti.demo.camunda.dmn.util.OrderHandlingResult;
  * This is a simple API to easily demonstrate how Camunda BPM can be intergrated in a Spring Boot based Microservice with REST API
  * API Design is quite awful (all GET with URL path variables, ...) but defined like this to easily demonstrate during a demo  
  */
-@Controller
+@RestController
 @RequestMapping("/order")
-@Produces("application/json")
 public class OrderController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
@@ -37,7 +32,7 @@ public class OrderController {
 	DecisionService decisionService;
 
     @GetMapping("/determine-handling/{previousOrders}/{daysSinceFirstOrder}/{valueOfOrder}")
-    public ResponseEntity<GenericResult> register(@PathVariable(name="previousOrders") Integer previousOrders, @PathVariable(name="daysSinceFirstOrder") Integer daysSinceFirstOrder, @PathVariable(name="valueOfOrder") Integer valueOfOrder, Model model) {
+    public GenericResult register(@PathVariable(name="previousOrders") Integer previousOrders, @PathVariable(name="daysSinceFirstOrder") Integer daysSinceFirstOrder, @PathVariable(name="valueOfOrder") Integer valueOfOrder, Model model) {
     	Map<String,Object> params = new HashMap<>();
     	params.put("previousOrders", previousOrders);
     	params.put("daysSinceFirstOrder", daysSinceFirstOrder);
@@ -50,7 +45,7 @@ public class OrderController {
     	Boolean gift = result.getFirstResult().getEntry("gift");
     	String label = result.getFirstResult().getEntry("customerLabel");
     	
-    	return new ResponseEntity<GenericResult>(new OrderHandlingResult(gift, label), HttpStatus.OK);
+    	return new OrderHandlingResult(gift, label);
     }
 
 }
